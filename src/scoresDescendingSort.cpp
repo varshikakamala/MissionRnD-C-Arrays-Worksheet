5c807e6b449705068e0dfa0c512d444531c9b8ed
varshikakamala <kamalareddy159@gmail.com>
2015-11-21 23:23:40 -0800

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -21,5 +21,39 @@ struct student {
 };
 
 void * scoresDescendingSort(struct student *students, int len) {
-	return NULL;
+	int i, j, temp, k, flag;
+	char temp1[10];
+	if (len < 0 || students == NULL)
+		return NULL;
+	else if (len > 1)
+	{
+		for (i = 1; i < len; i++)
+		{
+			if (students[0].score == students[i].score)
+				flag = 1;
+			else
+				flag = 0;
+		}
+		if (flag == 0)
+		{
+			for (i = 0; i < len-1; i++)
+			{
+				for (j = i+1 ; j < len; j++)
+				{
+					if (students[i].score < students[j].score)
+					{
+						temp = students[i].score;
+//						for (k = 0; students[i].name[k] != '\0'; k++)
+	//						temp1[k] = students[i].name[k];
+						students[i].score = students[j].score;
+		//				for (k = 0; students[j].name[k] != '\0';k++)
+			//				students[i].name[k] = students[j].name[k];
+						students[j].score = temp;
+				//		for (k = 0; temp1[k] != '\0';k++)
+					//		students[j].name[k] = temp1[k];
+					}
+				}
+			}
+		}
+	}
 }
