38076f6b883b9681bdd356b886ea5d3fca0b20d3
varshikakamala <kamalareddy159@gmail.com>
2015-12-07 07:21:27 -0800

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -15,12 +15,48 @@ NOTES:
 
 #include <iostream>
 #include <malloc.h>
+#include <stdio.h>
 
-struct student {
+struct student
+{
 	char *name;
 	int score;
 };
+struct student ** topKStudents(struct student *students, int len, int K)
+{
+	int i, j, score1;
+	char *name1;
 
-struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	struct student **std;
+	if (students == NULL || len<0 || K<1)
+		return NULL;
+	name1 = (char *)malloc(sizeof(char) * 5);
+
+	if (K > len)
+		K = len;
+	std = (struct student **)malloc(sizeof(struct student*)*K);
+
+	{
+		for (i = 0; i < len; i++)
+		{
+			for (j = 0; j < len; j++)
+			{
+				if (students[i].score >= students[j].score)
+				{
+					score1 = students[i].score;
+					students[i].score = students[j].score;
+					students[j].score = score1;
+
+					name1 = students[i].name;
+					students[i].name = students[j].name;
+					students[j].name = name1;
+
+				}
+			}
+		}
+		for (i = 0; i < K; i++)
+			std[i] = &students[i];
+		return std;
+	}
+
 }
